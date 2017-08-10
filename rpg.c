7715d3db7b7fcb72886e00e11256913348d65090
Trevor Stevenson <trevor.stevenson@duke.edu>
2017-08-09 23:45:03 -0400

    First story lines

--- rpg.c
+++ rpg.c
@@ -2,5 +2,8 @@
 
 int main(int argc, char *argv[])
 {
-    
+    printf("Your eyes open, and all you see is darkness.");
+    printf("Press 1: Go back to sleep.");
+    printf("Press 2: Look around.");
+    return 0;
 }
